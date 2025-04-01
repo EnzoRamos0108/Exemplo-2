b65738a1f2f9c0771415e4d78e07d1bd6b0932d4
Enzo <enzoframos10@gmail.com>
2025-04-01 09:27:53 -0300

    conversões

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,47 @@
-//DECLARAÇÕES E VARIÁVEIS
-var nome ="fiap";
-console.log(nome)
+// //DECLARAÇÕES E VARIÁVEIS
+// var nome ="fiap";
+// console.log(nome)
 
-let idade = 18;
-console.log(idade);
+// let idade = 18;
+// console.log(idade);
 
-const sobrenome = "Cidade"
-console.log (sobrenome)
+// const sobrenome = "Cidade"
+// console.log (sobrenome)
 
-let aula;
-console.log(aula)
+// let aula;
+// console.log(aula)
 
-let valor = null;
-console.log(valor)
+// let valor = null;
+// console.log(valor)
 
-let exemplo1={};
+// let exemplo1={};
 
-let exemplo2=[];
+// let exemplo2=[];
 
-//TIPOS VARIAVEIS
-let exemplo3 =10
-console.log(typeof exemplo3)
+// //TIPOS VARIAVEIS
+// let exemplo3 =10
+// console.log(typeof exemplo3)
 
-let exemplo4 = "Aula"
-console.log(typeof exemplo4)
+// let exemplo4 = "Aula"
+// console.log(typeof exemplo4)
 
-let exemplo5 = true
-console.log(typeof exemplo5)
+// let exemplo5 = true
+// console.log(typeof exemplo5)
 
-let exemplo6 =["Huguinho", "Zezinho" , "Luizinho"];
-console.log(typeof exemplo6)
+// let exemplo6 =["Huguinho", "Zezinho" , "Luizinho"];
+// console.log(typeof exemplo6)
 
 //CONVERSÕES
+
+let numFloat = 123.456;
+console.log(parseInt(numFloat));
+
+let numString= "547.987";
+console.log(parseFloat(numString));
+
+let numFLoat1=554.665;
+console.log(numFLoat1.toString());
+
+let numInt=100;
+console.log(numInt.toString());
+
