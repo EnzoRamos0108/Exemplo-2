45932ff89969cb0ae97e002f835ac54cec9b79a7
Enzo <enzoframos10@gmail.com>
2025-04-01 09:10:39 -0300

    Declarações e variáveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIÁVEIS
+var nome ="fiap";
+console.log(nome)
+
+let idade = 18;
+console.log(idade);
+
+const sobrenome = "Cidade"
+console.log (sobrenome)
+
+let aula;
+console.log(aula)
+
+let valor = null;
+console.log(valor)
+
+let exemplo1={};
+
+let exemplo2=[];
