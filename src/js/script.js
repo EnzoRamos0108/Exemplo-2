7c4124435d8ba15f5aa16b18a801290157232f1d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-08 08:46:55 -0300

    Adicionando operadores de comparação

--- src/js/script.js
+++ src/js/script.js
@@ -1,83 +1,91 @@
-// //DECLARAÇÕES E VARIÁVEIS
-// var nome ="fiap";
-// console.log(nome)
+// // //DECLARAÇÕES E VARIÁVEIS
+// // var nome ="fiap";
+// // console.log(nome)
 
-// let idade = 18;
-// console.log(idade);
+// // let idade = 18;
+// // console.log(idade);
 
-// const sobrenome = "Cidade"
-// console.log (sobrenome)
+// // const sobrenome = "Cidade"
+// // console.log (sobrenome)
 
-// let aula;
-// console.log(aula)
+// // let aula;
+// // console.log(aula)
 
-// let valor = null;
-// console.log(valor)
+// // let valor = null;
+// // console.log(valor)
 
-// let exemplo1={};
+// // let exemplo1={};
 
-// let exemplo2=[];
+// // let exemplo2=[];
 
-// //TIPOS VARIAVEIS
-// let exemplo3 =10
-// console.log(typeof exemplo3)
+// // //TIPOS VARIAVEIS
+// // let exemplo3 =10
+// // console.log(typeof exemplo3)
 
-// let exemplo4 = "Aula"
-// console.log(typeof exemplo4)
+// // let exemplo4 = "Aula"
+// // console.log(typeof exemplo4)
 
-// let exemplo5 = true
-// console.log(typeof exemplo5)
+// // let exemplo5 = true
+// // console.log(typeof exemplo5)
 
-// let exemplo6 =["Huguinho", "Zezinho" , "Luizinho"];
-// console.log(typeof exemplo6)
+// // let exemplo6 =["Huguinho", "Zezinho" , "Luizinho"];
+// // console.log(typeof exemplo6)
 
-//CONVERSÕES
+// //CONVERSÕES
 
-let numFloat = 123.456;
-console.log(parseInt(numFloat));
+// let numFloat = 123.456;
+// console.log(parseInt(numFloat));
 
-let numString= "547.987";
-console.log(parseFloat(numString));
+// let numString= "547.987";
+// console.log(parseFloat(numString));
 
-let numFLoat1=554.665;
-console.log(numFLoat1.toString());
+// let numFLoat1=554.665;
+// console.log(numFLoat1.toString());
 
-let numInt=100;
-console.log(numInt.toString());
+// let numInt=100;
+// console.log(numInt.toString());
 
-//METODOS - PARTE 1
+// //METODOS - PARTE 1
 
-//LENGHT - VERIFICA O TAMANHO DA STRING
+// //LENGHT - VERIFICA O TAMANHO DA STRING
 
-let frase = "O mundo da Tecnologia";
-console.log(frase.length)
+// let frase = "O mundo da Tecnologia";
+// console.log(frase.length)
 
-//indexOf - retorna um trecho de um texto
+// //indexOf - retorna um trecho de um texto
 
-let texto="Programação"
-console.log(texto.indexOf("a"))
+// let texto="Programação"
+// console.log(texto.indexOf("a"))
 
-//slice - retorna parte de um texto apontando o inicio
+// //slice - retorna parte de um texto apontando o inicio
 
-let info = "processamento de ponta";
-console.log(info.slice(0,13))
+// let info = "processamento de ponta";
+// console.log(info.slice(0,13))
 
-//OPERADORES ARITMÉTICOS
+// //OPERADORES ARITMÉTICOS
 
-const num1=10
-const num2=20
+// const num1=10
+// const num2=20
 
-console.log(num1 + num2)
-console.log(num1-num2)
-console.log (num1*num2)
-console.log(num1/num2)
+// console.log(num1 + num2)
+// console.log(num1-num2)
+// console.log (num1*num2)
+// console.log(num1/num2)
 
 //OPERADORES LÓGICOS
 
-const num3 = 20
-const num4=30
+// const num3 = 20
+// const num4=30
 
-console.log(num3>num4)
-console.log(num3<num4&& num4<50)
-console.log(num3 > num4 || num4 > num3)
-console.log(num3==num4 || num4<=num3)
+// console.log(num3>num4)
+// console.log(num3<num4&& num4<50)
+// console.log(num3 > num4 || num4 > num3)
+// console.log(num3==num4 || num4<=num3)
+
+const num5 = 25;
+const num6= 35;
+
+console.log(num5==num6) //compara
+console.log(num5===num6)//compara e verifica o tipo da variavel
+console.log(num5!=num6)//diferente
+
